a62a3644f925d246f23f75412ee052e9adbacd4e
antoniobrandi <antonio.brandi@pal-robotics.com>
2024-11-27 17:04:05 +0100

    use docking arg

--- pmb2_gazebo/launch/pmb2_gazebo.launch.py
+++ pmb2_gazebo/launch/pmb2_gazebo.launch.py
@@ -38,6 +38,7 @@ class LaunchArguments(LaunchArgumentsBase):
     navigation: DeclareLaunchArgument = CommonArgs.navigation
     slam: DeclareLaunchArgument = CommonArgs.slam
     advanced_navigation: DeclareLaunchArgument = CommonArgs.advanced_navigation
+    docking: DeclareLaunchArgument = CommonArgs.docking
     x: DeclareLaunchArgument = CommonArgs.x
     y: DeclareLaunchArgument = CommonArgs.y
     yaw: DeclareLaunchArgument = CommonArgs.yaw
@@ -109,6 +110,13 @@ def declare_actions(
         condition=IfCondition(LaunchConfiguration('advanced_navigation')))
 
     launch_description.add_action(advanced_navigation)
+
+    docking = include_scoped_launch_py_description(
+        pkg_name='pmb2_docking',
+        paths=['launch', 'pmb2_docking_bringup.launch.py'],
+        condition=IfCondition(LaunchConfiguration('docking')))
+
+    launch_description.add_action(docking)
 
     robot_spawn = include_scoped_launch_py_description(
         pkg_name='pmb2_gazebo',
